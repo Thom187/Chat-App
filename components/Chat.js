433aa404200db40a3ed31f079eda0731f78b8b4c
Thom187 <tom.hoppe@gmx.de>
2023-01-05 21:44:45 +0100

    Optimise Layout and Styling and add colour change

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class Chat extends React.Component {
 
   componentDidMount() {
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name })
+    this.props.navigation.setOptions({ title: name });
   }
 
   render() {
+    let color = this.props.route.params.color;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello Screen2!</Text>
+      <View style={[styles.container, { backgroundColor: color }]}>
+        <Text>Hello Chat!</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
