1800fa24c19e79ce0e3a6c939300f2170f48dec7
Thom187 <tom.hoppe@gmx.de>
2023-01-08 15:51:47 +0100

    Implement chat functionality
    User can write and send messages and a static initial message is shown

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
 
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    // set messages state with a static message to see UI elements
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hey developer',
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
   }
 
   render() {
     let color = this.props.route.params.color;
     return (
       <View style={[styles.container, { backgroundColor: color }]}>
-        <Text>Hello Chat!</Text>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
       </View>
     )
   }
@@ -21,7 +55,5 @@ export default class Chat extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
 })
