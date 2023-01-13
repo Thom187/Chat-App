431badbf98156bd38b1fc1d6b654b724230d65fe
Thom187 <tom.hoppe@gmx.de>
2023-01-13 17:15:21 +0100

    Pick images from uses's device/ upload to firebase

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -2,8 +2,58 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { connectActionSheet } from "@expo/react-native-action-sheet";
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class CustomActions extends React.Component {
+
+  // User can pick an image from the devices library
+  imagePicker = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    try {
+      if (status === 'granted') {
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes:
+            ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => console.log(error));
+
+        if (!result.canceled) {
+          const imageUrl = await this.uploadImageFetch(result.assets[0].uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Upload images to firebase
+  uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', uri, true);
+      xhr.send(null);
+    });
+    const imageNameBefore = uri.split('/');
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+    const ref = firebase.storage().ref().child(`${imageName}`);
+    const snapshot = await ref.put(blob);
+
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
+  };
 
   onActionPress = () => {
     const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
@@ -17,7 +67,7 @@ export default class CustomActions extends React.Component {
         switch (buttonIndex) {
           case 0:
             console.log('user wants to pick an image');
-            return;
+            return this.imagePicker();
           case 1:
             console.log('user wants to take a photo');
             return;
